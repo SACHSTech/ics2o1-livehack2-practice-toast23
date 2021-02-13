d787e9779eacdbbee0b9d0d3d8bc97114b96bd9d
Tony <tony.yao23@ycdsbk12.ca>
2021-02-13 07:42:33 +0000

    completed problem1.py and problem2.py

--- problem1.py
+++ problem1.py
@@ -1,1 +1,26 @@
+"""
+-------------------------------------------------------------------------------
+Name:   problem1.py
+Purpose:  calculate if triangle is right-angled
+ 
+Author: Yao.T
+ 
+Created:  13/02/2021
+------------------------------------------------------------------------------
+"""
 
+#get triangle information
+side1 = float(input("Enter the length of side 1: "))
+side2 = float(input("Enter the length of side 2: "))
+side3 = float(input("Enter the length of side 3: "))
+
+#square side lengths
+sq1 = side1 ** 2 
+sq2 = side2 ** 2 
+sq3 = side3 ** 2 
+
+#determine if triangle is right-angled, output
+if (sq1 + sq2 == sq3) or (sq1 + sq3 == sq2) or (sq2 + sq3 == sq1):
+  print(f"\nThe triangle with side lengths {side1}, {side2}, {side3} form a right-angled triangle.")
+else: 
+  print(f"\nThe triangle with side lengths {side1}, {side2}, {side3} does not form a right-angled triangle.")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,24 @@
+"""
+-------------------------------------------------------------------------------
+Name:   problem2.py
+Purpose:  determine if a vacation is rewarded 
+ 
+Author: Yao.T
+ 
+Created:  13/02/2021
+------------------------------------------------------------------------------
+"""
+
+#get mark and earnings information 
+mark = round(float(input("Enter mark: ")))
+earnings = float(input("Enter earnings: "))
+
+#determine if you can go on vacation, output
+if mark >= 80 and earnings >= 500:
+  print("\nYou can go to Europe!!!")
+elif mark >= 80 or earnings >= 500: 
+  print("\nYou can go to California.")
+else: 
+  print("\nYou can stay home!!! :)")
+
 
